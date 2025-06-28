18f05b9a6339c32b5e497a68d65f5c58b1a7be1d
Kajal15Shaw <kajalshaw28b@gmail.com>
2025-06-28 11:53:23 +0530

    commit

--- config/db.js
+++ config/db.js
@@ -3,14 +3,13 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    /*try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true
-        });
-        */
-
     try {
-        await mongoose.connect(db);
+        await mongoose.connect(db, {
+          useNewUrlParser: true,
+          useCreateIndex: true,
+          useFindAndModify: false,
+          useUnifiedTopology: true
+        });
 
         console.log('MongoDB connected..');
     } catch(err) {
