65f0973b1049c4190cda7a07d9c5b2921d177efd
Kajal15Shaw <kajalshaw28b@gmail.com>
2025-08-17 01:27:56 +0530

    git

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,14 @@
 const jwt = require('jsonwebtoken');
-const config = require('config');
 
 module.exports = function (req, res, next) {
-  // Get token from header
   const token = req.header('x-auth-token');
 
-  // Check if not token
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
-  // Verify token
   try {
-    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+    jwt.verify(token, process.env.jwtSecret, (error, decoded) => {
       if (error) {
         return res.status(401).json({ msg: 'Token is not valid' });
       } else {
@@ -21,7 +17,7 @@ module.exports = function (req, res, next) {
       }
     });
   } catch (err) {
-    console.error('something wrong with auth middleware');
+    console.error('Something went wrong with auth middleware');
     res.status(500).json({ msg: 'Server Error' });
   }
 };
